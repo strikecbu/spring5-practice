8f5e8c165098a7decdd1d239e4193feb77cee806
andychen <andy.chen@iisigroup.com>
2021-09-16 21:13:35 +0800

    Add dependency Lombok and Ingredient class

---
+++ src/main/java/tacos/Ingredient.java
@@ -0,0 +1,16 @@
+package tacos;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type {
+        WARP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
